9af446f9b911f3fe31acea7af2587609e7b64965
Grzegorz Stącel <stacelgrzegorz@gmail.com>
2021-07-01 11:15:53 +0200

    Add links to inner pages

--- src/views/Main/Main.js
+++ src/views/Main/Main.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import styles from './Main.module.scss';
 
 const Main = () => {
     return (
-        <p>This is an Main View</p>
+        <div className={styles.wrapper}>
+            <Link to='/czasu' className={styles.bigNav}>
+                Przyimki Czasu
+            </Link>
+            <Link to='/miejsca' className={styles.bigNav}>
+                Przyimki Miejsca
+            </Link>
+        </div>
     )
 }
 

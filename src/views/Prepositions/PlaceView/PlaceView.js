3202a7fa95e174f97a6d2d317e29c9e0d7eddcb9
Grzegorz Stącel <stacelgrzegorz@gmail.com>
2021-07-01 12:11:38 +0200

    Add view

--- src/views/Prepositions/PlaceView/PlaceView.js
+++ src/views/Prepositions/PlaceView/PlaceView.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import AppContext from '../../../components/context';
+import ListWrapper from '../../../components/ListWrapper/ListWrapper'
+import Button from '../../../components/Button/Button'
 
 const PlaceaView = () => {
     return (
-        <p>This is an Place View</p>
+        <AppContext.Consumer>
+            {(context) => (
+                <>
+                    <p>This is an Place View</p>
+                    <ListWrapper
+                        items={context.itemsPlace}
+                        placeOfArrayItems={0}
+                    />
+                    <Button
+                        setClassName={context.stylesResetButton}    
+                        handleOnClick={context.resetFn}
+                    >
+                        RESET
+                    </Button>
+                </>
+            )}
+        </AppContext.Consumer>
     )
 }
 
